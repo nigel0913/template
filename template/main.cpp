ea5e936b1d43a4335f47cd32cb398f9a92e63b60
nigel0913 <nigel0913@gmail.com>
2013-11-28 17:45:30 +0800

    add main.cpp stack1test.h stack1test.cpp

---
+++ template/main.cpp
@@ -0,0 +1,9 @@
+#include "stack1test.h"
+#include <cstdio>
+
+int main()
+{
+	test_stack();
+	getchar();
+	return 0;
+}
---
+++ template/stack1test.cpp
@@ -0,0 +1,25 @@
+#include "stack1test.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "stack1.hpp"
+
+void test_stack()
+{
+	try {
+		Stack<int> intStack;
+		Stack<std::string> stringStack;
+
+		intStack.push(7);
+		std::cout << intStack.top() << std::endl;
+
+		stringStack.push("hello");
+		std::cout << stringStack.top() << std::endl;
+		stringStack.pop();
+		stringStack.pop();
+	}
+	catch (std::exception const& ex) {
+		std::cerr << "Exception: " << ex.what() << std::endl;
+		return ;
+	}
+}
